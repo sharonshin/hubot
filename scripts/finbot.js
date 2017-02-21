a7060550290936deea7e7a5398eee9d61f4d3acc
Sharon Shin <sharonshin2@gmail.com>
2017-02-20 21:08:16 -0800

    add image

--- scripts/finbot.js
+++ scripts/finbot.js
@@ -10,8 +10,5 @@
     robot.hear(/Monday/, function(res) {
         return res.send("https://s-media-cache-ak0.pinimg.com/736x/9b/b3/5e/9bb35e437965f0d34bf288c853c64559.jpg");
     });
-
-    robot.respond(/What's your favorite color?/, function(res) {
-        return res.send("I may be biased, but I like purple.");
- });
+    
 };
