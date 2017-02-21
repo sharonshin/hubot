a5b6c468c3cc2208ffa3bad74d96d476e394d6b3
Sharon Shin <sharonshin2@gmail.com>
2017-02-20 20:35:20 -0800

    add monday image

--- scripts/finbot.js
+++ scripts/finbot.js
@@ -6,4 +6,8 @@
     robot.hear(/Hello!/, function(res) {
         return res.send("Hi there!");
     });
+
+    robot.hear(/Monday/, function(res) {
+        return img.src = https://s-media-cache-ak0.pinimg.com/736x/9b/b3/5e/9bb35e437965f0d34bf288c853c64559.jpg;
+    });
 };
