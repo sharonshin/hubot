c35fd5f37ee65c3d55e00fa94575a8f272ed6499
Sharon Shin <sharonshin2@gmail.com>
2017-02-21 17:19:16 -0800

    added weekend image

--- scripts/finbot.js
+++ scripts/finbot.js
@@ -1,10 +1,19 @@
  
  
-
+weekend = [
+    "http://connect.citizen.co.za/wp-content/uploads/sites/25/2016/05/WE2.jpg?x71574",
+    "http://lifepopper.com/wp-content/uploads/2015/02/Funny-friday-hilarious-lifepopper-pictures-love-weekend-fun-memes-6.jpg",
+    "http://cdn.pophangover.com/images/meme-912-1.jpg",
+    "https://cdn.meme.am/instances/56049068.jpg"
+]
 
  module.exports = function(robot) {
-    robot.hear(/Hello!/, function(res) {
-        return res.send("Hi there!");
+    robot.hear(/Hello|Hi|Hey/, function(res) {
+        return res.send("Hello friend!");
+    });
+
+    robot.hear(/Monday/, function(res) {
+        msg.send msg.random weekend;
     });
 
     robot.hear(/Monday/, function(res) {
